038565bab441970a2c04607f8d98b2900334a41b
Rodrigo Vidal <rguzman@miumg.edu.gt>
2025-09-14 11:31:05 -0600

    validación del nombre y manejo de errores

--- codigo.py
+++ codigo.py
@@ -3,7 +3,7 @@ import sys
 def saludar(nombre):
     print(f'Hola, {nombre}!')
 
-if len(sys.argv) > 1:
+if len(sys.argv) > 1 and sys.argv[1]:
     saludar(sys.argv[1])
 else:
-    saludar('mundo')
+    print('Error: No se proporcionó un nombre para saludar.')
