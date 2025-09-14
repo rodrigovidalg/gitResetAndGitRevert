931f28ee00d5de61a86c59950bee21dc57f141a1
Rodrigo Vidal <rguzman@miumg.edu.gt>
2025-09-14 11:53:49 -0600

    mensaje de saludo coloreado usando colorama

--- codigo.py
+++ codigo.py
@@ -1,9 +1,11 @@
 import sys
+from colorama import init, Fore
+init(autoreset=True)
 
 def saludar(nombre):
-    print(f'Hola, {nombre}!')
+    print(Fore.GREEN + f'Hola, {nombre}!')
 
 if len(sys.argv) > 1 and sys.argv[1]:
     saludar(sys.argv[1])
 else:
-    print('Error: No se proporcionó un nombre para saludar.')
+    print(Fore.RED + 'Error: No se proporcionó un nombre para saludar.')
